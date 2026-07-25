861630cae8ec629dbd2044f9d2f7a8e266738cf0
amirsaam <amirkamangar@icloud.com>
2026-07-25 17:43:40 +0330

    test(spacing): add spacing tests

--- Package.swift
+++ Package.swift
@@ -37,6 +37,7 @@ let package = Package(
             ],
             exclude: [
                 "Layout/SnapshotFixtures",
+                "Spacing/SnapshotFixtures",
                 "TWToken/SnapshotFixtures",
                 "Typography/SnapshotFixtures",
             ],
---
+++ Tests/ElementaryTailwindTests/Spacing/SpacingTests.swift
@@ -0,0 +1,22 @@
+import Elementary
+import ElementaryTailwind
+import TestUtilities
+import Testing
+
+struct SpacingTests {
+    @Test func testPadding() throws {
+        try HTMLAssertEqual(div(.padding(.size(4))) {}, try String(contentsOf: fixtureURL("padding.size-4.html"), encoding: .utf8))
+        try HTMLAssertEqual(div(.paddingX(.size(4))) {}, try String(contentsOf: fixtureURL("paddingX.size-4.html"), encoding: .utf8))
+        try HTMLAssertEqual(div(.paddingY(.size(4))) {}, try String(contentsOf: fixtureURL("paddingY.size-4.html"), encoding: .utf8))
+        try HTMLAssertEqual(div(.paddingTop(.size(4))) {}, try String(contentsOf: fixtureURL("paddingTop.size-4.html"), encoding: .utf8))
+        try HTMLAssertEqual(
+            div(.paddingBottom(.size(4))) {},
+            try String(contentsOf: fixtureURL("paddingBottom.size-4.html"), encoding: .utf8))
+    }
+
+    @Test func testMargin() throws {
+        try HTMLAssertEqual(div(.margin(.size(4))) {}, try String(contentsOf: fixtureURL("margin.size-4.html"), encoding: .utf8))
+        try HTMLAssertEqual(div(.marginX(.size(4))) {}, try String(contentsOf: fixtureURL("marginX.size-4.html"), encoding: .utf8))
+        try HTMLAssertEqual(div(.marginY(.auto)) {}, try String(contentsOf: fixtureURL("marginY.auto.html"), encoding: .utf8))
+    }
+}
